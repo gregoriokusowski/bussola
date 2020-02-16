7192f38c1053e58bc201ecaad829b0d5a8d5fedd
Gregorio Kusowski <gregorio.kusowski@gmail.com>
2020-02-16 19:39:34 +0100

    Add (br)ace editor

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
-});
+import * as ace from 'brace';
+import 'brace/mode/yaml';
+
+const editor = ace.edit('editor', {
+    mode: 'ace/mode/yaml',
+    selectionStyle: 'text'
+})
